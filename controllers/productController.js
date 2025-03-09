785032ec3992b4f15b4bd5a83523a9eb52dc158f
Felipe Chávez <136279896+Chavezgarrido@users.noreply.github.com>
2025-03-09 20:30:27 -0300

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -23,25 +23,29 @@ exports.getProductById = async (req, res) => {
 
         res.json(producto); 
     } catch (error) {
-        res.status(500).json({ error: 'Error al obtener el producto' });
+        res.status(500).json({ error: 'Error al obtener el producto', details: error.message });
     }
 };
 
 exports.getProductsByCategory = async (req, res) => {
     const { categoria } = req.params;
 
-    try{
+    try {
         const { data: productos, error } = await supabase
             .from('Productos')
             .select('*')
             .eq('categoria', categoria);
 
         if (error) {
-            return res.status(500).json({ error: 'Error al obtener productos', detailes: error.message });
+            return res.status(500).json({ error: 'Error al obtener productos', details: error.message });
+        }
+
+        if (!productos || productos.length === 0) {
+            return res.status(404).json({ error: 'No se encontraron productos en esta categoría' });
         }
 
         res.json(productos);
     } catch (error) {
-        res.status(500).json({ error: 'Error al obtener productos' });
+        res.status(500).json({ error: 'Error al obtener productos', details: error.message });
     }
 };
