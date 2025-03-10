01b44b2e0a11be6f08b6702f30b85d334d48eeb2
Felipe Chávez <136279896+Chavezgarrido@users.noreply.github.com>
2025-03-09 21:02:53 -0300

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', categoryController.getAllCategories);
 router.get('/:id', categoryController.getCategoryById);
-router.get('/categoria/productos/:categoria', categoryController.getProductsByCategory); 
+router.get('/productos/:categoria', categoryController.getProductsByCategory); 
 
 
 module.exports = router;
